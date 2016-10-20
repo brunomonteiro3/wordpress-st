69b9173c4c435f73191033051657ed4301575533
Bruno Monteiro <bruno.monteiro@empiricus.com.br>
2016-10-20 15:00:19 -0200

    Enabling footer scripts.

---
+++ components/foot.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+		* Default scripts of the webpage
+	*/
+	global $js;
+
+	get_scripts();
+	
+	wp_footer();
+?>
--- layouts/default.php
+++ layouts/default.php
@@ -1,4 +1,5 @@
 <?php
     the_component('components/head');    
     echo $args['content'];
+    the_component('components/foot');  
 ?>
